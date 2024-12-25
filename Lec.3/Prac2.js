3577377465bad9927cb7d642e471774eb333a5bf
ErwinJohannes <adithep817@gmail.com>
2024-12-25 11:30:48 +0700

    res

---
+++ Lec.3/Prac2.js
@@ -0,0 +1,9 @@
+let furniture = ['Table','Chairs','Couch']
+
+
+  for(let fur of furniture) {
+      console.log(fur);
+      for (let char of fur) {
+        console.log(char);
+      }
+  }
--- Lec.3/thisex.js
+++ Lec.3/thisex.js
@@ -4,9 +4,13 @@ const cat = {
     whatName() {
         return this.name;
     },
+    whatAge() {
+        return this.age;
+    },
 }
 
 console.log(cat.whatName());
 //output: shiro
 cat.name = "alex";
 console.log(cat.whatName());
+console.log(cat.whatAge());
