1a15178bdd5ae6aa52e6e2ab06f3f6c86101c23c
ErwinJohannes <adithep817@gmail.com>
2025-01-08 10:20:02 +0700

    4L

---
+++ Lec.4/libArea.mjs
@@ -0,0 +1,15 @@
+function areaTriangle(b,h){
+    returnb(b*h)/2;
+}
+
+function areaSqr(w,l){
+    return w*l;
+
+}
+
+function areaCircle(r){
+    return Math.PI * r*r ;
+
+}
+
+export{areaCircle,areaTriangle,areaSqr};
